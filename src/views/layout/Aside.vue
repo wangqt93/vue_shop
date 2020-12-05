816bfe8ea4ca88ab213d5d7f7d59d911e6df9a1a
gtxiaofei <gtxiaofei@100baimi.cn>
2020-12-05 18:09:21 +0800

    保存aside页面布局

---
+++ src/views/layout/Aside.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="aside-content">
+    <el-menu
+      v-for="item in listItems"
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>首页</span>
+        </template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+
+      </el-submenu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Aside',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="layout-content">
-    <el-button type="info" @click="logout">退出</el-button>
+    <el-container class="layout-container">
+
+      <el-header class="layout-header">
+        <div class="header-left">
+          <img src="@/assets/heima.png" alt="">
+          <span>后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout">退出</el-button>
+      </el-header>
+
+      <el-aside class="layout-aside">
+        <Aside></Aside>
+      </el-aside>
+
+      <el-container>
+        <el-main>
+
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import Aside from '@/views/layout/Aside'
 export default {
   name: 'index',
+  components: {
+    Aside
+  },
   data () {
     return {
     }
@@ -20,6 +43,31 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .layout-container {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    .layout-header {
+      height: 20px;
+      width: 100vw;
+      background: #eee;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 10px;
+      .header-left {
+        width: 300px;
+        display:flex;
+        justify-content: space-around;
+        align-items: center;
+        font-size: 18px;
+      }
+    }
+    .layout-aside {
+      width: 20vw;
+      height: 100vh;
+      background-color: #2b4b6b;
+    }
+  }
 </style>
